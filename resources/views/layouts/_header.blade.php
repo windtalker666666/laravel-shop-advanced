cb1e5509a15684c9e38089455e9e8c348cba51db
windtalker666666 <windtalker666666@gmail.com>
2018-10-12 06:20:25 +0000

    众筹技术逻辑

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="{{ url('/') }}">
-                Laravel Shop
+                太平五金门市
             </a>
         </div>
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
